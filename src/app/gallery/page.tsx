165a86108046438604c92448e971f8538ac91a1b
Abhishek Niraula <abhisekniraula321@gmail.com>
2024-08-08 09:59:59 +0545

    Changes2

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,7 @@
 import "@/src/app/globals.css";
 import HeaderTop from '@/src/components/HeaderTop';
 import Navbar from '@/src/components/Navbar';
-import Gallery from "./Gallery";
+import Gallery from "@/src/app/gallery/Gallery";
 import ClientSideWrapper from '@/src/components/ClientSideWrapper';
 
 export default function Home() {
