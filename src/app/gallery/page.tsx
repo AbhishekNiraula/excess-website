a7ea66d1a3f762cb56ef3ead9705b03773188c1b
Abhishek Niraula <159927008+AbhishekNiraula@users.noreply.github.com>
2024-08-14 18:53:57 +0545

    Gallery Environment Variable Setup

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -16,9 +16,9 @@ interface Image {
 }
 
 cloudinary.v2.config({
-  cloud_name: secrets.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: secrets.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: secrets.NEXT_PUBLIC_CLOUDINARY_API_SECRET, 
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
   secure: true,
 });
 
