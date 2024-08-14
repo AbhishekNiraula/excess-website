ba6f96d974765f5afb709f4edc2be4095fe7ede2
Abhishek Niraula <159927008+AbhishekNiraula@users.noreply.github.com>
2024-08-14 19:00:06 +0545

    Gallery Environment Variable Setup

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -19,7 +19,6 @@ cloudinary.v2.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
   api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
-  secure: true,
 });
 
 export default async function Home() {
