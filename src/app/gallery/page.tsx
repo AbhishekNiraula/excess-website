fcf3cf46eb5155ad41bea5cdb572b1afaa0d7ff3
Abhishek Niraula <159927008+AbhishekNiraula@users.noreply.github.com>
2024-08-14 18:45:47 +0545

    Gallery Environment Variable Setup

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -15,6 +15,13 @@ interface Image {
   asset_folder: string;
 }
 
+cloudinary.v2.config({
+  cloud_name: secrets.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: secrets.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: secrets.NEXT_PUBLIC_CLOUDINARY_API_SECRET, 
+  secure: true,
+});
+
 export default async function Home() {
   let res;
   try {
