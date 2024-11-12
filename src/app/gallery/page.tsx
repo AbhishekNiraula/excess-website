2d8454a05b2fa4252b994e79659a76f99f268ed2
Abhishek Niraula <080bei004@ioepc.edu.np>
2024-11-12 21:14:54 +0545

    Gallery Update - [Static]

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import View from "@/src/app/gallery/view";
+import View from '@/src/app/gallery/view';
 import HeaderTop from '@/src/components/HeaderTop';
 import Navbar from '@/src/components/Navbar';
 import cloudinary from 'cloudinary';
@@ -7,45 +7,47 @@ import '@/src/app/gallery/gallery.css';
 import '@/src/app/globals.css';
 import ClientSideWrapper from '@/src/components/ClientSideWrapper';
 import ErrorComponent from '@/src/app/error';
+import { promises as fs } from 'fs';
 
 interface Image {
-  public_id: string;
-  width: number;
-  height: number;
-  asset_folder: string;
+	public_id: string;
+	width: number;
+	height: number;
+	asset_folder: string;
 }
 export default async function Home() {
-  let res;
-  try {
-    res = await cloudinary.v2.search
-      .expression('resource_type:image')
-      .sort_by('public_id', 'desc')
-      .max_results(150)
-      .execute() as { resources: Image[] };
+	let data;
+	try {
+		const file = await fs.readFile(
+			process.cwd() + '/src/app/gallery/galleryImages.json',
+			'utf8'
+		);
+		data = JSON.parse(file);
+		console.log(data);
+	} catch (error) {
+		console.error('Error reading the data.', error);
+		return (
+			<ErrorComponent message="There was an error fetching the gallery images. Please check your internet connection or try again later." />
+		);
+	}
+	const images: Image[] = data.resources.map((value: any) => {
+		return {
+			public_id: value.public_id,
+			width: value.width,
+			height: value.height,
+			asset_folder: value.asset_folder,
+		};
+	});
+	return (
+		<>
+			<section id="header">
+				<HeaderTop />
+				<Navbar />
+			</section>
 
-  } 
-  catch (error) {
-    return (
-      <ErrorComponent message="There was an error fetching the gallery images. Please check your internet connection or try again later." />
-    );
-  }
-
-  if (!res || res.resources.length === 0) {
-    return (
-      <ErrorComponent message="No images found. Please check back later." />
-    );
-  }
-
-  return (
-    <>
-      <section id="header">
-        <HeaderTop />
-        <Navbar />
-      </section>
-
-      <ClientSideWrapper>
-        <View initialSearch="" images={res.resources} />
-      </ClientSideWrapper>
-    </>
-  );
+			<ClientSideWrapper>
+				<View initialSearch="" images={images} />
+			</ClientSideWrapper>
+		</>
+	);
 }
